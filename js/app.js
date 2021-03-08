e511ac6816245e4b14227672d90e5998b2b3113b
shalahuddin-ali <m.shalahuddin.ali@gmail.com>
2021-03-08 11:33:26 +0800

    added score and pellette

--- js/app.js
+++ js/app.js
@@ -17,17 +17,43 @@ $(() => {
 	let snakePos = { tr: 4, td: 5 };
 	let trValue = snakePos.tr;
 	let tdValue = snakePos.td;
-	console.log($("#tr" + trValue + "td" + tdValue));
+
+	let randPelletteTrVal = Math.floor(Math.random() * rowCell);
+	let randPelletteTdVal = Math.floor(Math.random() * rowCell);
+
+	let $score = $("div").attr("id", "score").text(0);
+	let currentScore = 0;
+
+	console.log(currentScore);
+
+	// random pellette
+	const randomPellette = () => {
+		randPelletteTrVal = Math.floor(Math.random() * rowCell);
+		randPelletteTdVal = Math.floor(Math.random() * rowCell);
+		let $drawPellete = $("#tr" + randPelletteTrVal + "td" + randPelletteTdVal);
+		$drawPellete.attr("class", "pellette");
+	};
+	randomPellette();
+
+	const updateScore = () => {
+		currentScore++;
+		$score = $("div").attr("id", "score").text(currentScore);
+	};
 
 	const drawSnake = () => {
-		let drawSnakePos = $("#tr" + trValue + "td" + tdValue);
-		drawSnakePos.attr("class", "snake");
+		if ($("#tr" + trValue + "td" + tdValue).attr("class") === "pellette") {
+			randomPellette(); // make new pellette once its being hit
+			updateScore();
+			console.log(currentScore);
+		}
+		let $drawSnakePos = $("#tr" + trValue + "td" + tdValue);
+		$drawSnakePos.attr("class", "snake");
 		console.log(trValue);
 		if (
-			trValue === -1 ||
-			trValue === rowCell ||
-			tdValue === -1 ||
-			tdValue === rowCell
+			trValue === -1 || // going outside of top border
+			trValue === rowCell || // going outside of bottom border
+			tdValue === -1 || // going outside of left border
+			tdValue === rowCell //going outside of right border
 		)
 			alert("GAME OVER!");
 	};
@@ -58,4 +84,26 @@ $(() => {
 				break;
 		}
 	});
+
+	// let randPelletteTrVal = Math.floor(Math.random() * rowCell);
+	// let randPelletteTdVal = Math.floor(Math.random() * rowCell);
+	// const randomPellette = () => {
+	// 	randPelletteTrVal = Math.floor(Math.random() * rowCell);
+	// 	randPelletteTdVal = Math.floor(Math.random() * rowCell);
+	// 	// console.log(randPelletteTdVal);
+	// 	// console.log(randPelletteTrVal);
+	// 	let $drawPellete = $("#tr" + randPelletteTrVal + "td" + randPelletteTdVal);
+	// 	$drawPellete.attr("class", "pellette");
+	// };
+	// randomPellette();
+	//hit pellette
+	//start button/reset
+	// score
+	// const updateScore = () => {
+	// 	currentScore++;
+	// 	$score = $("div").attr("id", "score").text(currentScore);
+	// };
+	//snake auto moves
+	//snake tails
+	//snake hit its own body
 });
